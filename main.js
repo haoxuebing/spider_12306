7d6f896626cb01e36b7412b45d7eb500d87ae21d
haoxuebing <haoxuebing@iyunxiao.com>
2018-01-11 18:54:53 +0800

    完善信息

--- main.js
+++ main.js
@@ -120,33 +120,33 @@ let questions = [
 function getLeftTicketUrl(callback) {
 	request.get("https://kyfw.12306.cn/otn/leftTicket/init", (e, r, b) => {
 		if (e) {
-			callback && callback({leftTicketUrl: 'leftTicket/query'});
+			callback && callback({ leftTicketUrl: 'leftTicket/query' });
 			console.log(e);
 			return;
 		}
-		$ = cheerio.load(r.body, {decodeEntities: false});
+		$ = cheerio.load(r.body, { decodeEntities: false });
 		let pageHtml = $.html();
 		let re = pageHtml.match(/var CLeftTicketUrl = '\w+\/\w+/ig);
 		let leftTicketUrl;
-		
+
 		if (re && re.length) {
 			leftTicketUrl = re[0].replace(/var CLeftTicketUrl = \'/, '');
-			
+
 			if (!leftTicketUrl) {
 				leftTicketUrl = 'leftTicket/query';
 			}
 		}
 		else {
-			leftTicketUrl = 'leftTicket/query';			
+			leftTicketUrl = 'leftTicket/query';
 		}
-		callback && callback({leftTicketUrl: leftTicketUrl});
+		callback && callback({ leftTicketUrl: leftTicketUrl });
 	});
 }
 fs.readFile('config.json', 'utf-8', function (err, data) {
 	if (err || !data || isRewrite) {
 		prompt(questions).then(answer => {
-			answer.from_station = _stations.stationInfo[answer.from_station];	
-			answer.end_station = _stations.stationInfo[answer.end_station];	
+			answer.from_station = _stations.stationInfo[answer.from_station];
+			answer.end_station = _stations.stationInfo[answer.end_station];
 			answer.train_num = answer.train_num.split('|');
 			answer.ticket_type = answer.ticket_type ? '0x00' : 'ADULT';
 			answer.receive_mail = answer.receive_mail || answer.your_mail;
@@ -176,22 +176,22 @@ fs.readFile('config.json', 'utf-8', function (err, data) {
 			});
 		});
 	}
-	
+
 });
 
-var yz_temp = [], yw_temp = [];//保存余票状态
+var ydz_temp = [], edz_temp = [], yw_temp = [], yz_temp = [], wz_temp = [];//保存余票状态
 /*
 * 查询余票
 */
 function queryTickets(config) {
 	/*设置请求头参数*/
 	let leftTicketUrl = config.leftTicketUrl;
-	console.log(leftTicketUrl);
+	console.log('Begin');
 	var options = {
 		hostname: 'kyfw.12306.cn',//12306
 		port: 443,
 		method: 'GET',
-		path: '/otn/'+leftTicketUrl+'?leftTicketDTO.train_date=' + config.time + '&leftTicketDTO.from_station=' + config.from_station.code + '&leftTicketDTO.to_station=' + config.end_station.code + '&purpose_codes=' + config.ticket_type,
+		path: '/otn/' + leftTicketUrl + '?leftTicketDTO.train_date=' + config.time + '&leftTicketDTO.from_station=' + config.from_station.code + '&leftTicketDTO.to_station=' + config.end_station.code + '&purpose_codes=' + config.ticket_type,
 		ca: [ca],//证书
 		rejectUnauthorized: false, //拒绝未经授权
 		headers: {
@@ -203,57 +203,7 @@ function queryTickets(config) {
 			"Cookie": "__NRF=D2A7CA0EBB8DD82350AAB934FA35745B; JSESSIONID=0A02F03F9852081DDBFEA4AA03EF4252C569EB7AB1; _jc_save_detail=true; _jc_save_showIns=true; BIGipServerotn=1072693770.38945.0000; _jc_save_fromStation=%u77F3%u5BB6%u5E84%2CSJP; _jc_save_toStation=%u5408%u80A5%2CHFH; _jc_save_fromDate=2017-02-17; _jc_save_toDate=2017-01-19; _jc_save_wfdc_flag=dc",
 		}
 	};
-	function b4(ct, cv) {
-		var cs = [];
-		for (var cr = 0; cr < ct.length; cr++) {
-			var cw = [];
-			var cq = ct[cr].split("|");
-			cw.secretHBStr = cq[36];
-			cw.secretStr = cq[0];
-			cw.buttonTextInfo = cq[1];
-			var cu = [];
-			cu.train_no = cq[2];
-			cu.station_train_code = cq[3];  //车次
-			cu.start_station_telecode = cq[4]; //始发站代码
-			cu.end_station_telecode = cq[5]; //终点站代码
-			cu.from_station_telecode = cq[6]; //出发地代码
-			cu.to_station_telecode = cq[7]; //目的地代码
-			cu.start_time = cq[8]; //开始时间
-			cu.arrive_time = cq[9]; //到达时间 
-			cu.lishi = cq[10]; //历时
-			cu.canWebBuy = cq[11]; //能买吗？
-			cu.yp_info = cq[12];
-			cu.start_train_date = cq[13]; //乘车日期
-			cu.train_seat_feature = cq[14];
-			cu.location_code = cq[15];  
-			cu.from_station_no = cq[16]; //始站序
-			cu.to_station_no = cq[17];  //到站序
-			cu.is_support_card = cq[18];
-			cu.controlled_train_flag = cq[19];
-			cu.gg_num = cq[20] ? cq[20] : "--";
-			cu.gr_num = cq[21] ? cq[21] : "--"; //高级软卧
-			cu.qt_num = cq[22] ? cq[22] : "--";
-			cu.rw_num = cq[23] ? cq[23] : "--"; //软卧
-			cu.rz_num = cq[24] ? cq[24] : "--"; //软座
-			cu.tz_num = cq[25] ? cq[25] : "--";
-			cu.wz_num = cq[26] ? cq[26] : "--"; //无座
-			cu.yb_num = cq[27] ? cq[27] : "--";
-			cu.yw_num = cq[28] ? cq[28] : "--"; //硬卧
-			cu.yz_num = cq[29] ? cq[29] : "--"; //硬座
-			cu.ze_num = cq[30] ? cq[30] : "--"; //二等座
-			cu.zy_num = cq[31] ? cq[31] : "--"; //一等座
-			cu.swz_num = cq[32] ? cq[32] : "--"; //商务座
-			cu.srrb_num = cq[33] ? cq[33] : "--"; //动卧
-			cu.yp_ex = cq[34];
-			cu.seat_types = cq[35];
-			cu.exchange_train_flag = cq[36];
-			cu.from_station_name = cv[cq[6]]; //出发地
-			cu.to_station_name = cv[cq[7]];  //目的地
-			cw.queryLeftNewDTO = cu;
-			cs.push(cw)
-		}
-		return cs
-	}
+
 	/*请求开始*/
 	var req = https.get(options, function (res) {
 		var data = '';
@@ -303,40 +253,48 @@ function queryTickets(config) {
 					console.log('当天没有' + train_arr[j] + '这趟车次');
 					continue;
 				}
-				var ydz=cur_train.zy_num; //一等座数目
-				var edz=cur_train.ze_num; //二等座数目
-				var yz = cur_train.yz_num; //硬座数目
+				var ydz = cur_train.zy_num; //一等座数目
+				var edz = cur_train.ze_num; //二等座数目
 				var yw = cur_train.yw_num; //硬卧数目
-				var wz=cur_train.wz_num; //站票数目
+				var yz = cur_train.yz_num; //硬座数目
+				var wz = cur_train.wz_num; //站票数目
 
 				var trainNum = cur_train.station_train_code;//车次
-				console.log('\n ' + trainNum + ' 车次的硬座余票数:', yz, ', 硬卧余票数:', yw, '。当前时间：' + getTime());
-				if (yz != '无' && yz != '--' || yw != '无' && yw != '--') {
-					if (yw_temp[j] == yw && yz_temp[j] == yz) {//当余票状态发生改变的时候就不发送邮件
-						console.log(' ' + trainNum + '车次状态没改变，不重复发邮件');
+
+				// console.log(trainNum + ' 车次的硬座余票数:', yz, ', 硬卧余票数:', yw, '。当前时间：' + getTime());
+
+				if (judgeState(ydz, edz, yz, yw, wz)) {
+					if (ydz_temp[j] == ydz && edz_temp[j] == edz && yw_temp[j] == yw && yz_temp[j] == yz && wz_temp[j] == wz) {//当余票状态发生改变的时候就不发送邮件
+						console.log(trainNum + '车次状态没改变，不重复发邮件');
 						continue;
 					}
-					var ticket_info=`,\n一等座：${ydz},\n二等座:${edz},\n硬卧：${yw},\n硬座：${yz},\n站票:${wz}`;
+					var ticket_info = `一等座：${ydz},\n二等座：${edz},\n硬卧：${yw},\n硬座：${yz},\n站票：${wz}`;
+					console.log(`${trainNum} ${ticket_info.replace(/\n/g, '')}`);
 					var mailOptions = {
 						from: config.your_mail, // 发件邮箱地址
 						to: config.receive_mail || config.your_mail, // 收件邮箱地址，可以和发件邮箱一样
-						subject: `${trainNum}有票啦`,// + '有票啦，硬座：' + yz + '，硬卧：' + yw, // 邮件标题
-						text: trainNum + '有票啦\n' + cur_train.from_station_name + '=============>' + cur_train.to_station_name + '\n出发日期：' + config.time + ',\n出发时间：' + cur_train.start_time + ',\n到达时间：' + cur_train.arrive_time + ',\n历时：' + cur_train.lishi+ticket_info, // 邮件内容
+						subject: `${trainNum}有票啦`, // 邮件标题
+						text: trainNum + '有票啦\n' + cur_train.from_station_name + '=======>' + cur_train.to_station_name + '\n出发日期：' + config.time + ',\n出发时间：' + cur_train.start_time + ',\n到达时间：' + cur_train.arrive_time + ',\n历时：' + cur_train.lishi + ',\n' + ticket_info, // 邮件内容
 					};
 
 					// 发邮件部分
-					(function (j, yz, yw) {
+					(function (j, ydz, edz, yw, yz, wz, trainNum) {
 						transporter.sendMail(mailOptions, function (error, info) {
 							if (error) {
 								return console.log(error);
 							}
-							console.log(' 邮件已发送: ====================> ' + mailOptions.to);
-							yw_temp[j] = yw;//保存当前列车的余票数量
+							console.log(`${trainNum}有票 & 邮件已发送: =======> ${mailOptions.to}`);
+
+							ydz_temp[j] = ydz;//保存当前列车的余票数量
+							edz_temp[j] = edz;
+							yw_temp[j] = yw;
 							yz_temp[j] = yz;
+							wz_temp[j] = wz;
+
 						});
-					})(j, yz, yw);
+					})(j, ydz, edz, yw, yz, wz, trainNum);
 				} else {
-					console.log(trainNum + '硬座/硬卧无票');
+					console.log(trainNum + '暂时无票');
 				}
 			}
 			// fs.writeFile('./train.json',data);
@@ -348,15 +306,21 @@ function queryTickets(config) {
 	});
 }
 
-/*
-* 爬取全国车站信息并生成JSON文件
-*/
+function judgeState(ydz, edz, yz, yw, wz) {
+	return getState(ydz) || getState(edz) || getState(yz) || getState(yw) || getState(wz);
+}
+
+function getState(val) {
+	return val != '无' && val != '--';
+}
+
+//爬取全国车站信息并生成JSON文件
 function stationJson() {
 	let _opt = {
 		hostname: 'kyfw.12306.cn',
 		path: '/otn/resources/js/framework/station_name.js?station_version=1.9042',
 		ca: [ca],
-		rejectUnauthorized: false		
+		rejectUnauthorized: false
 	};
 	let _data = '';
 	let _req = https.get(_opt, function (res) {
@@ -383,14 +347,14 @@ function stationJson() {
 					};
 					stationArray.push(t[3]);
 					if (!stationMap[t[3]]) {
-						stationMap[t[3]] = info;						
+						stationMap[t[3]] = info;
 					}
 					else {
 						if (Object.prototype.toString.call(stationMap[t[3]]) === '[object Array]') {
 							stationMap[t[3]] = [...stationMap[t[3]], info];
 						}
 						else {
-							stationMap[t[3]] = [stationMap[t[3]], info];						
+							stationMap[t[3]] = [stationMap[t[3]], info];
 						}
 					}
 				});
@@ -406,9 +370,62 @@ function stationJson() {
 		console.error(err.code);
 	});
 }
+
 function getTime() {
 	let T = new Date();
 	return T.getFullYear() + '-' + (parseInt(T.getMonth()) + 1) + '-' + T.getDate() + ' ' + T.getHours() + ":" + T.getMinutes() + ":" + T.getSeconds();
+}
+
+function b4(ct, cv) {
+	var cs = [];
+	for (var cr = 0; cr < ct.length; cr++) {
+		var cw = [];
+		var cq = ct[cr].split("|");
+		cw.secretHBStr = cq[36];
+		cw.secretStr = cq[0];
+		cw.buttonTextInfo = cq[1];
+		var cu = [];
+		cu.train_no = cq[2];
+		cu.station_train_code = cq[3];  //车次
+		cu.start_station_telecode = cq[4]; //始发站代码
+		cu.end_station_telecode = cq[5]; //终点站代码
+		cu.from_station_telecode = cq[6]; //出发地代码
+		cu.to_station_telecode = cq[7]; //目的地代码
+		cu.start_time = cq[8]; //开始时间
+		cu.arrive_time = cq[9]; //到达时间 
+		cu.lishi = cq[10]; //历时
+		cu.canWebBuy = cq[11]; //能买吗？
+		cu.yp_info = cq[12];
+		cu.start_train_date = cq[13]; //乘车日期
+		cu.train_seat_feature = cq[14];
+		cu.location_code = cq[15];
+		cu.from_station_no = cq[16]; //始站序
+		cu.to_station_no = cq[17];  //到站序
+		cu.is_support_card = cq[18];
+		cu.controlled_train_flag = cq[19];
+		cu.gg_num = cq[20] ? cq[20] : "--";
+		cu.gr_num = cq[21] ? cq[21] : "--"; //高级软卧
+		cu.qt_num = cq[22] ? cq[22] : "--";
+		cu.rw_num = cq[23] ? cq[23] : "--"; //软卧
+		cu.rz_num = cq[24] ? cq[24] : "--"; //软座
+		cu.tz_num = cq[25] ? cq[25] : "--";
+		cu.wz_num = cq[26] ? cq[26] : "--"; //无座
+		cu.yb_num = cq[27] ? cq[27] : "--";
+		cu.yw_num = cq[28] ? cq[28] : "--"; //硬卧
+		cu.yz_num = cq[29] ? cq[29] : "--"; //硬座
+		cu.ze_num = cq[30] ? cq[30] : "--"; //二等座
+		cu.zy_num = cq[31] ? cq[31] : "--"; //一等座
+		cu.swz_num = cq[32] ? cq[32] : "--"; //商务座
+		cu.srrb_num = cq[33] ? cq[33] : "--"; //动卧
+		cu.yp_ex = cq[34];
+		cu.seat_types = cq[35];
+		cu.exchange_train_flag = cq[36];
+		cu.from_station_name = cv[cq[6]]; //出发地
+		cu.to_station_name = cv[cq[7]];  //目的地
+		cw.queryLeftNewDTO = cu;
+		cs.push(cw)
+	}
+	return cs
 }
 
 
